87aacb947c9d795aead7a0296b700c71c039b10a
Danny Warren <danny.warren@emmersionlearning.com>
2020-04-13 18:55:14 -0600

    Create Manage Page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <a href="/">Manage</a>
+        <a href="/">Preview</a>
+      </nav>
+      <div className="crud-area">
+        <section>
+          <h2>Shows</h2>
+          <div>
+            <div>
+              Show 1 <button>(delete)</button>
+            </div>
+            <div>
+              Show 2 <button>(delete)</button>
+            </div>
+          </div>
+        </section>
+        <main>
+          <h2>New/Edit Show</h2>
+          <div>
+            <label htmlFor="name-input">Name:</label>
+            <input id="name-input" type="text"/>
+          </div>
+          <div>
+            <label htmlFor="rating-input">Rating:</label>
+            <input id="rating-input" type="text"/>
+          </div>
+          <div>
+            <label htmlFor="image-url-input">Image URL:</label>
+            <input id="image-url-input" type="text"/>
+          </div>
+          <button>Save</button>
+        </main>
+      </div>
     </div>
   );
 }
